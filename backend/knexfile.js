0a593707428cf4da368001a62ea8da0c853afe2c
mirandaeverton <mir.everton@gmail.com>
2022-05-29 15:38:05 +0100

    Add migrations

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,47 +1,16 @@
-// Update with your config settings.
-
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+  client: 'postgresql',
+  connection: {
+    database: 'knowledge',
+    user: 'postgres',
+    password: 'Mirp0stgr3s0205-'
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  pool: {
+    min: 2,
+    max: 10
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  migrations: {
+    tableName: 'knex_migrations'
   }
 
 };
