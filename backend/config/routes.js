024d399117220d1a3cd7d7f6ef1f48beb7f3f290
mirandaeverton <mir.everton@gmail.com>
2022-06-01 15:22:42 +0100

    Create auth API. Add signin and validateToken methods to auth API.

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,4 +1,7 @@
 module.exports = app => {
+    app.post('/signup', app.api.user.save)
+    app.post('/signin', app.api.auth.signIn)
+    app.post('/validateToken', app.api.auth.validateToken)
     app.route('/users')
         .post(app.api.user.save)
         .get(app.api.user.get)
