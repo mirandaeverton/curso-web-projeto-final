b8c56ced50ea777b9193065965d22300b253bc25
mirandaeverton <mir.everton@gmail.com>
2022-06-01 15:39:18 +0100

    Add passport protection/authentication

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -2,34 +2,43 @@ module.exports = app => {
     app.post('/signup', app.api.user.save)
     app.post('/signin', app.api.auth.signIn)
     app.post('/validateToken', app.api.auth.validateToken)
+
     app.route('/users')
+        .all(app.config.passport.authenticate())
         .post(app.api.user.save)
         .get(app.api.user.get)
 
     app.route('/users/:id')
+        .all(app.config.passport.authenticate())
         .put(app.api.user.save)
         .get(app.api.user.getById)
 
     app.route('/categories')
+        .all(app.config.passport.authenticate())
         .post(app.api.category.save)
         .get(app.api.category.get)
 
     app.route('/categories/tree')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getTree)
 
     app.route('/categories/:id')
+        .all(app.config.passport.authenticate())
         .put(app.api.category.save)
         .get(app.api.category.getById)
         .delete(app.api.category.remove)
 
     app.route('/articles')
+        .all(app.config.passport.authenticate())
         .post(app.api.article.save)
         .get(app.api.article.get)
 
     app.route('/articles/:id')
+        .all(app.config.passport.authenticate())
         .put(app.api.article.save)
         .get(app.api.article.getById)
     
     app.route('/categories/:id/articles')
+        .all(app.config.passport.authenticate())
         .get(app.api.article.getByCategory)
 }
