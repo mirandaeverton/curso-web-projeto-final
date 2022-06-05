4f3c8518fd3d90fd1184f54a305a0af660faea00
mirandaeverton <mir.everton@gmail.com>
2022-06-05 12:11:40 +0100

    Fixes error if password sent is not a string

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -9,16 +9,17 @@ module.exports = app => {
         if(!req.body.email || !req.body.password) {
             return res.status(400).send('Informe usuário e senha')
         }
-
+        
         const user = await app.db('users')
-            .where({ email: req.body.email })
-            .first()
+        .where({ email: req.body.email })
+        .first()
         
         if(!user) return res.status(400).send('Usuário não encontrado!')
 
-        const isMatch = bcrypt.compareSync(req.body.password, user.password)
+        const password = req.body.password.toString()
+        const isMatch = bcrypt.compareSync(password, user.password)
         if(!isMatch) return res.status(401).send('Usuário/Senha inválidos!')
-
+        
         const now = Math.floor(Date.now() / 1000)
 
         const payload = {
