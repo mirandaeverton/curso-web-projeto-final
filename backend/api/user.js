37e49dae43915358aeec9d8baf8b2daf9e3ed29d
mirandaeverton <mir.everton@gmail.com>
2022-06-06 19:30:50 +0100

    Fixes the issue where admin users could be created from public routes

--- backend/api/user.js
+++ backend/api/user.js
@@ -14,6 +14,9 @@ module.exports = app => {
             user.id = req.params.id
         }
 
+        if(!req.originalUrl.startsWith('/users')) user.admin = false
+        if(!req.user || !req.user.admin) user.admin = false
+
         try{
             existsOrError(user.name, 'Nome não informado')
             existsOrError(user.email, 'E-mail não informado')
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -7,10 +7,8 @@ module.exports = app => {
 
     app.route('/users')
         .all(app.config.passport.authenticate())
-        // .post(admin(app.api.user.save))
-        // .get(admin(app.api.user.get))
-        .post(app.api.user.save)
-        .get(app.api.user.get)
+        .post(admin(app.api.user.save))
+        .get(admin(app.api.user.get))
 
     app.route('/users/:id')
         .all(app.config.passport.authenticate())
