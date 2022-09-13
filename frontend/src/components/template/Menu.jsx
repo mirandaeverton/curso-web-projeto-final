05fd7d49d91365750c8852588c4005497c015a39
mirandaeverton <mir.everton@gmail.com>
2022-09-13 22:49:01 +0100

    Create function to build the menu tree

--- frontend/src/components/template/Menu.jsx
+++ frontend/src/components/template/Menu.jsx
@@ -1,7 +1,10 @@
 import styles from '../../styles/template/Menu.module.css'
+import MenuTree from './MenuTree'
 
 export default function Menu(props) {
     return(
-        <aside className={styles.menu}></aside>
+        <aside className={styles.menu}>
+            <MenuTree/>
+        </aside>
     )
 }
